f438f17cea99d24f4e848e656e89e3bb90a2c18a
arfiravy0 <arfiraandayani1000@gmail.com>
2024-10-16 20:34:00 +0700

    first commit

---
+++ flutter_new_proyek/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_new_proyek/home_page.dart';
+import 'package:flutter_new_proyek/login_page.dart';
+import 'package:flutter_new_proyek/register_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login/Register Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      initialRoute: '/',
+      routes: {
+        '/' : (context)=>LoginPage(),
+        '/register' : (context)=>RegisterPage(),
+        '/home' : (context)=>HomePage()
+      },
+    );
+  }
+}
---
+++ flutter_new_proyek/lib/register_page.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_new_proyek/login_page.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+final formKey = GlobalKey<FormState>();
+final namaLengkapController = TextEditingController();
+final emailController = TextEditingController();
+final passwordController = TextEditingController();
+final confirmPasswordController = TextEditingController();
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daftar',
+        style: TextStyle(color: Colors.white),),
+        backgroundColor: const Color.fromARGB(255, 235, 158, 247),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.person, size: 100, color: const Color.fromARGB(255, 243, 151, 214)),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Nama Lengkap',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if(value==null||value.isEmpty) {
+                      return 'Nama Lengkap tidak boleh kosong';
+                    }
+                    return null;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if(value==null||value.isEmpty) {
+                      return 'Email tidak boleh kosong';
+                  }
+                  if (!RegExp(r'^[^@]+@[^@]+.[^@]+').hasMatch(value)) {
+                    return 'Masukkan email yang valid';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if(value==null||value.isEmpty) {
+                      return 'Password tidak boleh kosong';
+                  } 
+                  if (value.length<6) {
+                    return 'Masukkan Passwort yang valid';
+                  }
+                  return null;
+                },
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Konfirmasi Password',
+                  border: OutlineInputBorder(),
+                ),
+                 validator: (value) {
+                  if(value==null||value.isEmpty) {
+                      return 'Konfirmasi Password tidak boleh kosong';
+                  } 
+                  if (value!=passwordController.text) {
+                    return 'Konfirmasi Password tidak cocok';
+                  }
+                  return null;
+                },
+                obscureText: true,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    Navigator.pushNamed(context, '/');
+                  }
+                },
+                child: Text('Daftar'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Sudah punya akun? Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
